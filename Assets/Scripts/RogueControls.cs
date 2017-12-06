ca61a21027e3edbc0c4887996eb1116557d9b601
MaximumCarnage <dylansbruton@gmail.com>
2017-12-05 20:37:31 -0500

    rogue setup start

---
+++ Assets/Scripts/RogueControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogueControls : MonoBehaviour {
+	public Transform m_ultiTarget;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.K)){
+			gameObject.transform.position = m_ultiTarget.position;
+		}
+	}
+}
