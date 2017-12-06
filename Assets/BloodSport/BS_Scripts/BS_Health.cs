1ad41d5ba5a2dedf480b9e9f9b2f78f3e12ecb78
Josh Brackett <joshua.brackett@hotmail.ca>
2017-12-05 19:13:09 -0500

    added health script and folder structure

---
+++ Assets/BloodSport/BS_Scripts/BS_Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BS_Health : MonoBehaviour {
+
+	public const float MAX_HEALTH = 1000.0f;
+	public float m_currentHealth;
+
+	void start() {
+		m_currentHealth = MAX_HEALTH;
+	}
+
+	public void TakeDamage(float damage) {
+		m_currentHealth -= damage;
+		Die();
+	}
+
+	public void Die() {
+		if(m_currentHealth <= 0) {
+			m_currentHealth = 0;
+			// TODO: add what happens when they die
+			Debug.Log("Dead");
+		}
+	}
+
+	public void Heal(int amount) {
+		m_currentHealth += amount;
+		if(m_currentHealth > MAX_HEALTH) {
+			m_currentHealth = MAX_HEALTH;
+		}
+	}
+}
