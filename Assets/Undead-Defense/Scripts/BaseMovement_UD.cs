ce8ef163f285a3513686cab26487db4135667b6b
Jason Berry <mrberry1990@gmail.com>
2017-12-06 15:29:46 -0500

    base player character that can look around with the mouse

---
+++ Assets/Undead-Defense/Scripts/BaseMovement_UD.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement_UD : MonoBehaviour {
+
+	public float m_speed;
+	
+	public void Move(Vector3 direction) {
+		direction *= (Time.deltaTime * m_speed);
+		transform.Translate(direction.x,0,direction.z);
+	}
+}
---
+++ Assets/Undead-Defense/Scripts/CharacterMovement_UD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMovement_UD : MonoBehaviour {
+
+	public float m_rotationSpeed = 180;
+	public Transform m_imagePos;
+
+	private BaseMovement_UD m_movement;
+
+	void Start () {
+		Camera.main.GetComponent<BS_ThirdPersonCamera>().Target = transform;
+		m_movement = GetComponent<BaseMovement_UD>();
+	}
+	
+	void Update () {
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+		m_movement.Move(movement.normalized);
+		transform.rotation = RotateWithCursor();
+		DrawCursor();
+	}
+
+	Quaternion RotateWithCursor(){
+		Vector3 rotation = transform.rotation.eulerAngles;
+		rotation.y += Input.GetAxis("Mouse X") * m_rotationSpeed * Time.deltaTime;
+		return Quaternion.Euler(rotation);
+	}
+
+	void DrawCursor() {
+		Vector2 point = new Vector2(Screen.width*0.5f, Screen.height*0.5f);
+		m_imagePos.position = point;
+	}
+}
