68f6571bd377d87b78682a4668b28fa183f14c43
Josh Brackett <joshua.brackett@hotmail.ca>
2017-12-07 20:22:15 -0500

    added sword collider and damage for warrior sword

--- Assets/BloodSport/BS_Scripts/BS_WarriorSwordCollider.cs
+++ Assets/BloodSport/BS_Scripts/BS_WarriorSwordCollider.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 public class BS_WarriorSwordCollider : MonoBehaviour {
 
 	private float m_damage = 10.0f;
-	public Animator m_animController;
-
-	void Start() {
-		m_animController = GetComponent<Animator>();
-	}
 
 	void OnTriggerEnter(Collider other) {
 		if(other.name == "Enemy") {
 			BS_Health healthScript = other.GetComponent<BS_Health>();
 			if(healthScript != null) {
-				if(a)
 				healthScript.TakeDamage(m_damage);
 				Debug.Log(healthScript.m_currentHealth);
 			}
