a71af1f72e097b1d49847013285ae92520f0fdc2
Josh Brackett <joshua.brackett@hotmail.ca>
2017-12-07 20:00:31 -0500

    changed health scripts max health to not be a const

--- Assets/BloodSport/BS_Scripts/BS_Health.cs
+++ Assets/BloodSport/BS_Scripts/BS_Health.cs
@@ -5,23 +5,27 @@ using UnityEngine.UI;
 
 public class BS_Health : MonoBehaviour {
 
-	public const float MAX_HEALTH = 1000.0f;
+	public float m_maxHealth = 1000.0f;
 	public float m_currentHealth;
 	public float m_damageReduction = 1f;
 	public Slider m_healthBar;
 	public Text m_healthText;
 
 	void Start() {
-		m_currentHealth = MAX_HEALTH;
-		m_healthBar.value = CalculateHealth();
-		m_healthText.text = m_currentHealth + "/" + MAX_HEALTH;
+		m_currentHealth = m_maxHealth;
+		if(m_healthBar != null && m_healthText != null) {
+			m_healthBar.value = CalculateHealth();
+			m_healthText.text = m_currentHealth + "/" + m_maxHealth;
+		}
 	}
 
 	public void TakeDamage(float damage) {
 		m_currentHealth -= damage * m_damageReduction;
 		Die();
-		m_healthBar.value = CalculateHealth();
-		m_healthText.text = m_currentHealth + "/" + MAX_HEALTH;
+		if(m_healthBar != null && m_healthText != null) {
+			m_healthBar.value = CalculateHealth();
+			m_healthText.text = m_currentHealth + "/" + m_maxHealth;
+		}
 	}
 
 	public void Die() {
@@ -34,14 +38,16 @@ public class BS_Health : MonoBehaviour {
 
 	public void Heal(int amount) {
 		m_currentHealth += amount;
-		if(m_currentHealth > MAX_HEALTH) {
-			m_currentHealth = MAX_HEALTH;
+		if(m_currentHealth > m_maxHealth) {
+			m_currentHealth = m_maxHealth;
 		}
-		m_healthBar.value = CalculateHealth();
-		m_healthText.text = m_currentHealth + "/" + MAX_HEALTH;
+		if(m_healthBar != null && m_healthText != null) {
+			m_healthBar.value = CalculateHealth();
+			m_healthText.text = m_currentHealth + "/" + m_maxHealth;
+		}
 	}
 
 	private float CalculateHealth() {
-		return m_currentHealth / MAX_HEALTH;
+		return m_currentHealth / m_maxHealth;
 	}
 }
