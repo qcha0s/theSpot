defb52abebb007ff36c55606a7b5d287203ee42d
Maaz Haq <Maaz_haq123@hotmail.com>
2017-12-09 19:28:28 -0500

    Cleaned up branch

---
+++ Assets/Game_BloodSport/Scripts/BS_WarriorSwordCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BS_WarriorSwordCollider : MonoBehaviour {
+
+	private float m_damage = 10.0f;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.name == "Enemy") {
+			BS_Health healthScript = other.GetComponent<BS_Health>();
+			if(healthScript != null) {
+				healthScript.TakeDamage(m_damage);
+				Debug.Log(healthScript.m_currentHealth);
+			}
+		}
+	}
+}
