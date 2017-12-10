e6758066d206290b579eeb6298b90aa191e66131
Tom Sauer <tom@exluto.com>
2017-12-09 21:32:02 -0500

    trying to resolve many conflicts

---
+++ Assets/Game_RabbitRacing/Scripts/CRTEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent( typeof( Camera ) )]
+public class CRTEffect : MonoBehaviour {
+	public Material m_material;
+	public float m_tuningStrength = 0.5f;
+	public float m_gradingResolution = 16f;
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+
+		m_material.SetFloat( "Tuning_Strength", m_tuningStrength);
+		m_material.SetFloat( "GradingRes", m_gradingResolution );
+
+		Graphics.Blit( src, dest, m_material );
+	}
+}
---
+++ Assets/Game_RabbitRacing/Scripts/MysteryBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MysteryBox : MonoBehaviour
+{
+    
+    public Transform[] PowerUps;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //other.gameObject.GetComponent<ItemSlot>().SetItem(PowerUps[Random.Range(0, PowerUps.Length)]);
+            Destroy(gameObject);
+        }
+    }
+
+ 
+}
