36cff720e71ae9d417f28808c7c4e09e56b67841
KevinXue <ivexnuke@gmail.com>
2017-12-11 17:06:30 -0500

    Created test item and projectile without tracking

---
+++ Assets/RabbitRacing/Scripts/UntrackedProjectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UntrackedProjectile : MonoBehaviour, Item {
+	public float m_travelSpeed = 30.0f;
+	public void Use(GameObject user){
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		transform.rotation = user.transform.rotation;
+		rigidbody.velocity = Vector3.forward * m_travelSpeed;
+	}
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
