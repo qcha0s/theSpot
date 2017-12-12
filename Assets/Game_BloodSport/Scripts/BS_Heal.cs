b007ebad92208896a4393dc97e9773773e65633b
Claydel <firesedge@hotmail.com>
2017-12-11 20:24:03 -0500

    adding health pickup mechanic

---
+++ Assets/Game_BloodSport/Scripts/BS_Heal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BS_Heal : MonoBehaviour {
+	private BS_PlayerHealth playerHealth; 
+void OnTriggerEnter(Collider other){
+		
+		Debug.Log("heal");
+			playerHealth = other.GetComponent<BS_PlayerHealth>();
+		if(playerHealth == null){
+			return;
+
+
+		}else{
+			
+			Debug.Log(playerHealth == null);
+			playerHealth.PlayerHeal(100);
+			Destroy(gameObject);
+		}
+}
+}
