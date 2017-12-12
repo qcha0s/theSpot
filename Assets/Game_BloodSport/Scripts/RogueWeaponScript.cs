d915f2a151eb98a9bbdfd7ffde8a54a9c01ef716
MaximumCarnage <dylansbruton@gmail.com>
2017-12-11 19:19:39 -0500

    readding dagger prefab matierials,wallowing in self pity

---
+++ Assets/Game_BloodSport/Scripts/RogueWeaponScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogueWeaponScript : MonoBehaviour {
+
+	OnTriggerEnter(Collider other){
+		//if(other.tag)
+
+	}
+}
