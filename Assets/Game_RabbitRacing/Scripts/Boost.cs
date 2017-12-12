34b48594e41aade826abb72640e48a50bd15444f
Emilie Hepburn <emilie@studiohepburn.com>
2017-12-11 20:03:06 -0500

    Created Boost Powerup Script

---
+++ Assets/Game_RabbitRacing/Scripts/Boost.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour, Item{
+
+    //when powerup is activivated increase speed for a period of time.
+    private float timerLength = 5f; 
+    private float timerTimePassed = 0f;
+    private BaseKartMovement m_baseKartMovement;
+    private float m_speed;
+    private float m_acc;
+    bool runTimer = false;
+
+
+    public void Use(GameObject user)
+    {
+        runTimer = true;
+        m_baseKartMovement = GetComponent<BaseKartMovement>();
+        m_speed = m_baseKartMovement.m_maxSpeed;
+        m_acc = m_baseKartMovement.m_acceleration;
+        m_baseKartMovement.m_maxSpeed *= 1.5f;
+        m_baseKartMovement.m_acceleration *= 1.5f;
+    }
+
+    void Update()
+    {
+        if (runTimer)
+        {
+            timerTimePassed += Time.deltaTime;
+           
+            if (timerTimePassed >= timerLength)
+            {
+                timerTimePassed = 0f;
+                runTimer = false;
+                m_baseKartMovement.m_maxSpeed = m_speed;
+                m_baseKartMovement.m_acceleration = m_acc;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
