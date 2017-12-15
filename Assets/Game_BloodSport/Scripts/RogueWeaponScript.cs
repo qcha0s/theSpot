80128b8a51fac09b0a700a22f1d75702f57fa140
MaximumCarnage <dylansbruton@gmail.com>
2017-12-14 19:36:00 -0500

    slight weapon tweaks

--- Assets/Game_BloodSport/Scripts/RogueWeaponScript.cs
+++ Assets/Game_BloodSport/Scripts/RogueWeaponScript.cs
@@ -11,9 +11,6 @@ public class RogueWeaponScript : MonoBehaviour {
 
    private float m_totalPoisonTime=2;
 
-   void Update(){
-       Debug.Log(m_poisoned);
-   }
 	void OnTriggerEnter(Collider other){
 		if(other.tag == "target"){
 			 Vector3 toTarget = (other.transform.position - transform.position).normalized;
@@ -21,18 +18,14 @@ public class RogueWeaponScript : MonoBehaviour {
             if(m_poisoned){
                 other.GetComponent<BS_Health>().TakeDotDamage(m_poisonDmg,m_poisonCount,m_totalPoisonTime);
             }
-
              if (Vector3.Dot(toTarget, other.transform.forward) > 0) {
-                 Debug.Log("Player Behind Object.");
                  other.GetComponent<BS_Health>().TakeDamage(m_weapDmg*2);
              } else {
-                 Debug.Log("Player in front Object.");
                  other.GetComponent<BS_Health>().TakeDamage(m_weapDmg);
              }
 		}
 
 	}
-
     public void SetPoison(bool ispoisoned){
         m_poisoned = ispoisoned;
         
