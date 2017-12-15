548d378acdda1b466dec02b2fcb65353eb5b728b
Brent Page <Brentpage75@gmail.com>
2017-12-15 12:39:13 -0500

    created puck, arena and player 2 prefabs

--- Assets/Game_TronLazerz/Scripts/TronPlayerMovement.cs
+++ Assets/Game_TronLazerz/Scripts/TronPlayerMovement.cs
@@ -15,10 +15,13 @@ public class TronPlayerMovement : MonoBehaviour {
         void Update(){
             if (Input.GetMouseButton(0))
             {
+            Cursor.visible = false;
             Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
             Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
             transform.position = objPosition;
+            
+            }else{
+                Cursor.visible = true;
             }
         }
    
