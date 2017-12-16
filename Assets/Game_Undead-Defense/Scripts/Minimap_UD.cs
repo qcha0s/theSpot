25225a93b8b3212d0d971eeede7e79661553fd40
Nue <undefinedhoujuunue@gmail.com>
2017-12-15 19:42:12 -0500

    Minimap

---
+++ Assets/Game_Undead-Defense/Scripts/Minimap_UD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//resize the minimap to a square
+
+public class Minimap_UD : MonoBehaviour {
+
+	Camera m_minimapCamera;
+
+	void Awake(){
+		m_minimapCamera = GetComponent<Camera>();
+
+		float height = m_minimapCamera.pixelRect.height;
+
+		Rect pixelRect = m_minimapCamera.pixelRect;
+
+		pixelRect.width = height;
+		pixelRect.x = (float)Screen.width - height;
+
+		m_minimapCamera.pixelRect = pixelRect;
+		Debug.Log(m_minimapCamera.pixelRect);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
