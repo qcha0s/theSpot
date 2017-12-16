8625bdfddac8b9bc4d1707d5716166e57e14f941
Brent Page <Brentpage75@gmail.com>
2017-12-15 22:02:48 -0500

    changing puck colors based on who is leading

--- Assets/Game_TronLazerz/Scripts/ScoreManager.cs
+++ Assets/Game_TronLazerz/Scripts/ScoreManager.cs
@@ -12,7 +12,11 @@ public class ScoreManager : MonoBehaviour {
 	public bool scored_Red;
 	public bool scored_Blue;
 
-	public Rigidbody puck;
+	public Rigidbody red_puck;
+	public Rigidbody blue_puck;
+	public Rigidbody green_puck;
+
+	public float hockey_Time;
 
 
 
@@ -23,13 +27,38 @@ public class ScoreManager : MonoBehaviour {
 		score_Red = 0;
 		scored_Blue = false;
 		scored_Red = false;
+		hockey_Time = 10;
 		
 	}
 
 	void RespawnPuck(){
-			Rigidbody clone;
-            clone = Instantiate(puck, transform.position, transform.rotation) as Rigidbody;
+			Rigidbody clone;            
+				if(score_Blue > score_Red){
+					clone = Instantiate(blue_puck, transform.position, transform.rotation) as Rigidbody;
+				}
+				if(score_Blue < score_Red){
+					clone = Instantiate(red_puck, transform.position, transform.rotation) as Rigidbody;
+				}
+				if(score_Blue == score_Red){
+					clone = Instantiate(green_puck, transform.position, transform.rotation) as Rigidbody;
+				}
 		
+	}
+	void Overtime(){
+		hockey_Time = 10;
+	}
+
+	void HockeyTimer(){
+		hockey_Time = hockey_Time -= Time.deltaTime;
+			if(hockey_Time <= 0 && score_Red > score_Blue){
+				RedWins();
+			}
+			if(hockey_Time <= 0 && score_Blue > score_Red){
+				BlueWins();
+			}
+			if(hockey_Time == 0 && score_Blue == score_Red){
+				Overtime();
+			}
 	}
 
 	void RedScore() {
@@ -69,6 +98,7 @@ public class ScoreManager : MonoBehaviour {
 			RedScore();
 			RespawnPuck();
 		}
-		
+		HockeyTimer();
+		Debug.Log(hockey_Time);
 	}
 }
