eb2c4457a28681e8f15105186f51458032313539
MaximumCarnage <dylansbruton@gmail.com>
2017-12-18 02:57:05 -0500

    Merging BloodSpot into branch, fixing nomenclature

---
+++ Assets/Game_BloodSport/Scripts/States/StateManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates { INTRO, MENU, PLAY, WON, LOST }
+
+
+public class StateManager : MonoBehaviour {
+
+	
+	public GameObject[] m_gameStates; 
+	private GameStates m_activeState; 
+	private int m_numStates;
+
+	void Start() {
+		m_numStates = m_gameStates.Length;
+		
+		for(int i = 0; i < m_numStates; i++) {
+			m_gameStates[i].SetActive(false);
+		}
+
+		m_activeState = GameStates.INTRO;
+		m_gameStates[(int)m_activeState].SetActive(true); // 0 == menu	
+		BS_GameManager.Instance.StartGame();
+	}
+
+	public void ChangeState(GameStates newState){
+		m_gameStates[(int)m_activeState].SetActive(false);
+		m_activeState = newState;
+		m_gameStates[(int)m_activeState].SetActive(true);
+	}
+
+
+	public void PlayGame() {
+		BS_GameManager.Instance.m_stateGameMenu.PlayGame();
+		m_gameStates[(int)m_activeState].SetActive(false);
+		m_activeState = GameStates.PLAY;
+		m_gameStates[(int)m_activeState].SetActive(true);
+	}
+
+	public void QuitGame() {
+		BS_GameManager.Instance.m_stateGameMenu.QuitGame();
+	}
+
+
+}
+
+
