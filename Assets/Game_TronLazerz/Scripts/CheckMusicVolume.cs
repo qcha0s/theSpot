46d2f8a5d185636c48c4043d764dd981edba3816
DKG333 <32583614+DKG333@users.noreply.github.com>
2017-12-18 19:25:04 -0500

    re did menu system

---
+++ Assets/Game_TronLazerz/Scripts/CheckMusicVolume.cs
@@ -0,0 +1,18 @@
+﻿// Converted from UnityScript to C# at http://www.M2H.nl/files/js_to_c.php - by Mike Hergaarden
+// Do test the code! You usually need to change a few small bits.
+
+using UnityEngine;
+using System.Collections;
+
+public class CheckMusicVolume : MonoBehaviour {
+
+
+void  Start (){
+	// remember volume level from last time
+	this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
+}
+
+void  UpdateVolume (){
+	this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
+}
+}
