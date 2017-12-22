9b8c4152da0bd65536b0f08f9807f20aebd5ffcc
Alex Rochon <alex1rochon23@outlook.com>
2017-12-21 21:06:11 -0500

    Alterations to Boost script, some trees added to Forest Stage

--- Assets/Game_RabbitRacing/Scripts/Items/Boost.cs
+++ Assets/Game_RabbitRacing/Scripts/Items/Boost.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boost : MonoBehaviour, Item{
+public class Boost : MonoBehaviour, Item {
 
     //when powerup is activivated increase speed for a period of time.
+    private GameObject m_player;
     private float timerLength = 5f; 
     private float timerTimePassed = 0f;
     private CharacterControllerKart m_baseKartMovement;
@@ -12,11 +13,24 @@ public class Boost : MonoBehaviour, Item{
     private float m_maxSpeed;
     bool runTimer = false;
 
+    public void SetPlayer(GameObject player) {
+        Debug.Log("Player being set");
+        m_player = player;
+        m_baseKartMovement = m_player.GetComponent<CharacterControllerKart>();
+        Debug.Log(gameObject.name);
+    }
 
     public void Use(GameObject user)
     {
+        Debug.Log(gameObject.name);
+        if (m_player == null) {
+            Debug.Log("No player");
+        } else {
+            Debug.Log("Have player");
+        }
         runTimer = true;
-        m_baseKartMovement = GetComponent<CharacterControllerKart>();
+        Debug.Log(m_baseKartMovement == null);
+        //m_baseKartMovement = m_player.GetComponent<CharacterControllerKart>();
         m_acc = m_baseKartMovement.Acceleration;
         m_maxSpeed = m_baseKartMovement.MaxSpeed;
         m_acc = 25f;
@@ -37,7 +51,7 @@ public class Boost : MonoBehaviour, Item{
                 runTimer = false;
                 m_acc = 8f;
                 m_maxSpeed = 25f;
-                //m_baseKartMovement.m_acceleration = m_acc;
+                m_baseKartMovement.m_acceleration = m_acc;
                 Destroy(gameObject);
             }
         }
