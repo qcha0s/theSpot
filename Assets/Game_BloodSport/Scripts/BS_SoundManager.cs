4abf416de5400742e0fd7803710a91c305e9c413
MaximumCarnage <dylansbruton@gmail.com>
2018-01-03 18:15:02 -0500

    starting soundManager

---
+++ Assets/Game_BloodSport/Scripts/BS_SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BS_SoundManager : MonoBehaviour {
+
+	public AudioClip mage_BH;
+	public AudioClip mage_Fire;
+	public AudioClip mage_Poly;
+	public AudioClip mage_Blink;
+
+	public AudioClip warrior_ChargeHit;
+	public AudioClip warrior_ArmorHit;
+	public AudioClip warrior_Whirlwind;
+	public AudioClip warrior_Sword;
+
+
+	public AudioClip rogue_BackStab;
+	public AudioClip rogue_Poison;
+	public AudioClip rogue_SmokeBomb;
+	public AudioClip rogue_Dagger;
+
+
+}
