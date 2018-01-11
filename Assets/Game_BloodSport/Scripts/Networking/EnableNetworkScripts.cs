cce315ba9a0140434563af498f893fe9f9dcb1a1
Josh Brackett <joshua.brackett@hotmail.ca>
2018-01-11 18:01:35 -0500

    add a fix to EnableNetworkScript where it wasnt enabling RPGCharacterController

--- Assets/Game_BloodSport/Scripts/Networking/EnableNetworkScripts.cs
+++ Assets/Game_BloodSport/Scripts/Networking/EnableNetworkScripts.cs
@@ -13,20 +13,23 @@ public class EnableNetworkScripts : NetworkBehaviour {
 
 	void Start() {
 		if(isLocalPlayer) {
-			GetComponent<RPGCharacterController>().enabled = true;
 			
 			switch(m_chosenChar) {
 				case 0:
 					m_mage.GetComponent<BS_Mage>().enabled = true;
+					m_mage.GetComponent<RPGCharacterController>().enabled = true;
 					break;
 				case 1:
 					m_warrior.GetComponent<BS_Warrior>().enabled = true;
+					m_warrior.GetComponent<RPGCharacterController>().enabled = true;
 					break;
 				case 2:
 					m_rogue.GetComponent<BS_Rogue>().enabled = true;
+					m_rogue.GetComponent<RPGCharacterController>().enabled = true;
 					break;
 				case 3:
 					m_hunter.GetComponent<BS_Hunter>().enabled = true;
+					m_hunter.GetComponent<RPGCharacterController>().enabled = true;
 					break;
 				default:
 					Debug.Log("Char not found");
