e9d3ce5febdee656d34c213062ed5aa1d187a5c7
Josh Brackett <joshua.brackett@hotmail.ca>
2018-01-11 17:55:50 -0500

    Added a network script to set player movement scripts active if you're the local player

---
+++ Assets/Game_BloodSport/Scripts/Networking/EnableNetworkScripts.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnableNetworkScripts : NetworkBehaviour {
+
+	public int m_chosenChar = 0;
+	public GameObject m_mage;
+	public GameObject m_warrior;
+	public GameObject m_rogue;
+	public GameObject m_hunter;
+
+	void Start() {
+		if(isLocalPlayer) {
+			GetComponent<RPGCharacterController>().enabled = true;
+			
+			switch(m_chosenChar) {
+				case 0:
+					m_mage.GetComponent<BS_Mage>().enabled = true;
+					break;
+				case 1:
+					m_warrior.GetComponent<BS_Warrior>().enabled = true;
+					break;
+				case 2:
+					m_rogue.GetComponent<BS_Rogue>().enabled = true;
+					break;
+				case 3:
+					m_hunter.GetComponent<BS_Hunter>().enabled = true;
+					break;
+				default:
+					Debug.Log("Char not found");
+					break;
+			}
+		}
+	}
+}
