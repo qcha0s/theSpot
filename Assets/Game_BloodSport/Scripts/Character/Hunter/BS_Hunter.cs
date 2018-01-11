9db6b8ed41c9d30463571ea844a6837ba442124a
MaximumCarnage <dylansbruton@gmail.com>
2018-01-10 20:29:42 -0500

    Making My lord and Savior Happy

--- Assets/Game_BloodSport/Scripts/Character/Hunter/BS_Hunter.cs
+++ Assets/Game_BloodSport/Scripts/Character/Hunter/BS_Hunter.cs
@@ -7,12 +7,12 @@ public class BS_Hunter : MonoBehaviour {
 	public GameObject m_ArrowPrefab;
 	public GameObject m_ArrowSpawn;
 	public Transform m_ArrowHandler;
-	
 	public Image[] m_CDMasks;
 	public GameObject m_Ultimate;
 	public GameObject m_IceArrowPrefab;
 	public GameObject m_FlameArrow;
 	public BS_SoundManager m_soundMgr;
+
 	private float m_ArrowVelocity = 500;
 	private bool m_ultActive = true;
 	private bool m_isUlting = false;
@@ -22,40 +22,30 @@ public class BS_Hunter : MonoBehaviour {
 	private float m_iceCD=6f;
 	private float m_ultarrowcount = 20;
 	private float m_DisCD = 10f;
-	private Vector3 m_DisengageVec = new Vector3(0,2f,-3f);
-
+	private Vector3 m_DisengageVec = new Vector3(0, 2f, -3f);
 	private RPGCharacterController m_characterController;
 
-	// Use this for initialization
 	void Start () {
 		m_characterController = GetComponent<RPGCharacterController>();
 		m_animationController = GetComponent<Animator>();
-		for(int i = 0; i < m_CDMasks.Length;i++){
-			m_CDMasks[i].fillAmount=0;
+		for(int i = 0; i < m_CDMasks.Length; i++){
+			m_CDMasks[i].fillAmount = 0;
 			Color temp = m_CDMasks[i].color;
-			temp.a=0.7f;
-			m_CDMasks[i].color=temp;
+			temp.a = 0.7f;
+			m_CDMasks[i].color = temp;
 		}
 	}
 	
-	// Update is called once per frame
 	void Update () {
-
 		if(Input.GetMouseButtonDown(0)) {
 			m_animationController.SetTrigger("isAttacking");
 			ArrowFire();	
 		}
-	
-
-
 		if(Input.GetKeyDown(KeyCode.Alpha1) && !m_iceOnCD) {
 			IceArrow();
 		}
 		if(Input.GetKeyDown(KeyCode.Alpha2) && !m_disOnCD) {
 			Disengage();
-		}
-		if(Input.GetKeyDown(KeyCode.Alpha3) && m_ultActive) {
-			Ultimate();
 		}
 	}
 
@@ -64,10 +54,8 @@ public class BS_Hunter : MonoBehaviour {
 		var bullet = (GameObject)Instantiate (m_ArrowPrefab, m_ArrowHandler.position, m_ArrowHandler.rotation);
 		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
 		Destroy(bullet, 2.0f);
-		
-
-
 	}
+
 	// Ice arrow does more damage than normal arrow
 	void IceArrow() {
 		m_soundMgr.PlayArrowShot();
@@ -78,70 +66,66 @@ public class BS_Hunter : MonoBehaviour {
 		m_iceOnCD = true;
 		StartCoroutine(CoolDownSystem(m_iceCD,"Ice"));
 	}
+
 	// flame arrow  does damage over time to the target
 	void Disengage() {
 		m_soundMgr.PlayDisengage();
-		m_animationController.SetBool("Movement",true);
-		m_characterController.m_Disengage=true;
+		m_animationController.SetBool("Movement", true);
+		m_characterController.m_Disengage = true;
 		m_CDMasks[1].fillAmount = 1;
 		m_disOnCD = true;
-		StartCoroutine(CoolDownSystem(m_DisCD,"Disengage"));
+		StartCoroutine(CoolDownSystem(m_DisCD, "Disengage"));
 	}
+
 	// ultimate create a volley of arrows that drop on enemies and do increased damage
 	public void Ultimate() {
-		Debug.Log(" pressed ult");
-		m_animationController.SetBool("IsUlting",true);
+		m_animationController.SetBool("IsUlting", true);
 		StartCoroutine(StartUlt());
 	}
+
 	IEnumerator StartUlt(){
-		
 		m_isUlting = true;
 		while(m_isUlting){
 			m_characterController.m_disableMovement = true;
-			Quaternion m_randrotate = new Quaternion(m_ArrowHandler.rotation.w,m_ArrowHandler.rotation.x,Random.Range(-20,20),m_ArrowHandler.rotation.z);
+			Vector3 bulletRotation = transform.rotation.eulerAngles;
+			bulletRotation += new Vector3(0, Random.Range(-20, 20), 0);
 			m_soundMgr.PlayArrowShot();
-			var bullet = (GameObject)Instantiate (m_ArrowPrefab, m_ArrowHandler.position,m_randrotate);
+			var bullet = (GameObject)Instantiate (m_ArrowPrefab, m_ArrowHandler.position, Quaternion.Euler(bulletRotation));
 			bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
 			Destroy(bullet, 2.0f);
 			m_ultarrowcount--;
-			Debug.Log(m_randrotate);
+			
 			if(m_ultarrowcount == 0f){
 				m_isUlting = false;
 			}
 			yield return new WaitForSeconds(0.1f);
 		}
 		m_ultarrowcount = 20;
-		m_animationController.SetBool("IsUlting",false);
+		m_animationController.SetBool("IsUlting", false);
 		m_characterController.m_disableMovement = false;
 		yield return null;
 	}
+	
 	IEnumerator CoolDownSystem(float cooldownvalue, string Ability){
 			if(Ability == "Ice"){
 			
 			while(m_iceOnCD){
-				m_CDMasks[0].fillAmount-=Time.deltaTime/cooldownvalue;
-
-				if(m_CDMasks[0].fillAmount==0){
+				m_CDMasks[0].fillAmount -= Time.deltaTime / cooldownvalue;
+				if(m_CDMasks[0].fillAmount == 0){
 					m_iceOnCD = false;
 				}
 				yield return null;
-				
 			}
 		}
 		
 		if(Ability == "Disengage"){
 			while(m_disOnCD){
-				m_CDMasks[1].fillAmount-=Time.deltaTime/cooldownvalue;
-
-				if(m_CDMasks[1].fillAmount==0){
+				m_CDMasks[1].fillAmount -= Time.deltaTime / cooldownvalue;
+				if(m_CDMasks[1].fillAmount == 0){
 					m_disOnCD = false;
 				}
 				yield return null;
-				
 			}
-			
 		}
-	
-		
 	}
 }
--- Assets/Game_BloodSport/Scripts/Character/Rogue/BS_Rogue.cs
+++ Assets/Game_BloodSport/Scripts/Character/Rogue/BS_Rogue.cs
@@ -11,6 +11,7 @@ public class BS_Rogue : MonoBehaviour {
 	public Material m_poisonMats;
 	public Material m_daggerBase;
 	public BS_SoundManager m_soundMgr;
+	
 	private bool m_UltActive = true;
 	private bool m_sprintOnCD = false;
 	private bool m_poisonOnCD = false;
@@ -35,8 +36,6 @@ public class BS_Rogue : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		
 		if(m_characterController.m_walkByDefault == false){
 			m_animator.SetBool("Movement",true);
 		}
@@ -49,25 +48,19 @@ public class BS_Rogue : MonoBehaviour {
 			m_weaponHitBoxes[0].enabled = true;
 			m_weaponHitBoxes[1].enabled = true;
 		}
-		
-
 		if(Input.GetKey(KeyCode.Alpha1) && !m_poisonOnCD){
 			m_soundMgr.PlayPoison();
 			Poison();
 		}
-
 		 if(Input.GetKey(KeyCode.Alpha2) && !m_sprintOnCD ){
 			Sprint();
-		}else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-			
 		}
 	}
 	public void Sprint(){
-				m_sprintOnCD = true;
-				m_CDMasks[1].fillAmount=1;
-				StartCoroutine(CoolDownSystem(m_sprintCD,"Sprint"));
-				StartCoroutine(StartSprint());
-			
+		m_sprintOnCD = true;
+		m_CDMasks[1].fillAmount=1;
+		StartCoroutine(CoolDownSystem(m_sprintCD,"Sprint"));
+		StartCoroutine(StartSprint());
 	}
 
 	public void Poison(){
@@ -79,34 +72,34 @@ public class BS_Rogue : MonoBehaviour {
 		m_poisonOnCD = true;
 		StartCoroutine(CoolDownSystem(m_poisonCD,"Poison"));
 		StartCoroutine(StartPoison());
-		
-		
 	}
+
 	public void UltTarget(){
 		m_soundMgr.PlaySmoke();
 		m_targetGUI.SetActive(true);
 	}
+
 	public void ShadowStep(Transform targetLocation){
 		gameObject.SetActive(false);
 		gameObject.transform.position = targetLocation.position;
 		gameObject.SetActive(true);
 		m_targetGUI.SetActive(false);
 	}
+
 	IEnumerator StartSprint(){
 		m_characterController.m_walkByDefault=false;
 		yield return new WaitForSeconds(4);
 		m_characterController.m_walkByDefault=true;
-		
-		
 	}
-	IEnumerator StartPoison(){
 
+	IEnumerator StartPoison(){
 		yield return new WaitForSeconds(3);
 		m_weapons[0].GetComponent<MeshRenderer>().material = m_daggerBase;
 		m_weapons[1].GetComponent<MeshRenderer>().material = m_daggerBase;
 		m_weapons[0].GetComponent<RogueWeaponScript>().SetPoison(false);
 		m_weapons[1].GetComponent<RogueWeaponScript>().SetPoison(false);
 	}
+
 	IEnumerator CoolDownSystem(float cooldownvalue, string Ability){
 			if(Ability == "Poison"){
 			Debug.Log("StartCD");
@@ -117,7 +110,6 @@ public class BS_Rogue : MonoBehaviour {
 					m_poisonOnCD = false;
 				}
 				yield return null;
-				
 			}
 		}
 		
@@ -129,11 +121,7 @@ public class BS_Rogue : MonoBehaviour {
 					m_sprintOnCD = false;
 				}
 				yield return null;
-				
 			}
-			
 		}
-	
-		
 	}
 }
