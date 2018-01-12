27f997fa5df4fafe63ce42831d7328d3e03c9bcb
Maaz Haq <Maaz_haq123@hotmail.com>
2018-01-12 03:34:59 -0500

    got the controller and avatar for the animator to switch on runtime made placeholder animators to make thing easier

--- Assets/Game_BloodSport/Scripts/Networking/EnableNetworkScripts.cs
+++ Assets/Game_BloodSport/Scripts/Networking/EnableNetworkScripts.cs
@@ -5,11 +5,16 @@ using UnityEngine.Networking;
 
 public class EnableNetworkScripts : NetworkBehaviour {
 
-	public int m_chosenChar = 0;
+	private int m_chosenChar = 0; //may need to change to public
 	public GameObject m_mage;
 	public GameObject m_warrior;
 	public GameObject m_rogue;
 	public GameObject m_hunter;
+	public GameObject HunterAnim;
+	public GameObject MageAnim;
+	public GameObject RogueAnim;
+	public GameObject WarriorAnim;
+
 
 	public GameObject m_overallMage;
 	public GameObject m_overallWarrior;
@@ -21,6 +26,7 @@ public class EnableNetworkScripts : NetworkBehaviour {
 	private int m_mageWarriorNum = 8;
 	private int m_rogueHunterNum = 7;
 	private Animator m_currAnimator;
+	private Animator m_classAnimator;
 
 	void Awake() {
 		m_netAnim = this.GetComponent<NetworkAnimator>();
@@ -29,44 +35,75 @@ public class EnableNetworkScripts : NetworkBehaviour {
 
 	public override void OnStartLocalPlayer() {
 		//m_netAnim.enabled = true;
-			switch(m_chosenChar) {
-				case 0:
-					m_mage.GetComponent<BS_Mage>().enabled = true;
-					m_mage.GetComponent<RPGCharacterController>().enabled = true;
-					//m_currAnimator = m_mage.GetComponent<Animator>();
-					m_animCounter = m_mageWarriorNum;
-					//m_overallMage.SetActive(true);
-					break;
-				case 1:
-					m_warrior.GetComponent<BS_Warrior>().enabled = true;
-					m_warrior.GetComponent<RPGCharacterController>().enabled = true;
-					//m_currAnimator = m_warrior.GetComponent<Animator>();
-					m_animCounter = m_mageWarriorNum;
-					//m_overallWarrior.SetActive(true);
-					break;
-				case 2:
-					m_rogue.GetComponent<BS_Rogue>().enabled = true;
-					m_rogue.GetComponent<RPGCharacterController>().enabled = true;
-					//m_currAnimator = m_rogue.GetComponent<Animator>();
-					m_animCounter = m_rogueHunterNum;
-					//m_overallRogue.SetActive(true);
-					break;
-				case 3:
-					m_hunter.GetComponent<BS_Hunter>().enabled = true;
-					m_hunter.GetComponent<RPGCharacterController>().enabled = true;
-					//m_currAnimator = m_hunter.GetComponent<Animator>();
-					m_animCounter = m_rogueHunterNum;
-					//m_overallHunter.SetActive(true);
-					break;
-				default:
-					Debug.Log("Char not found");
-					break;
-			}
+		switch(m_chosenChar) {
+			case 0:
+				m_mage.GetComponent<BS_Mage>().enabled = true;
+				m_mage.GetComponent<RPGCharacterController>().enabled = true;
+				//m_currAnimator = m_mage.GetComponent<Animator>();
+				m_animCounter = m_mageWarriorNum;
+				break;
+			case 1:
+				m_warrior.GetComponent<BS_Warrior>().enabled = true;
+				m_warrior.GetComponent<RPGCharacterController>().enabled = true;
+				//m_currAnimator = m_warrior.GetComponent<Animator>();
+				m_animCounter = m_mageWarriorNum;
+				//m_overallWarrior.SetActive(true);
+				break;
+			case 2:
+				m_rogue.GetComponent<BS_Rogue>().enabled = true;
+				m_rogue.GetComponent<RPGCharacterController>().enabled = true;
+				//m_currAnimator = m_rogue.GetComponent<Animator>();
+				m_animCounter = m_rogueHunterNum;
+				//m_overallRogue.SetActive(true);
+				break;
+			case 3:
+				m_hunter.GetComponent<BS_Hunter>().enabled = true;
+				m_hunter.GetComponent<RPGCharacterController>().enabled = true;
+				//m_currAnimator = m_hunter.GetComponent<Animator>();
+				m_animCounter = m_rogueHunterNum;
+				//m_overallHunter.SetActive(true);
+				break;
+			default:
+				Debug.Log("Char not found");
+				break;
+		}
 		SetAnimator();	
 	}
 
 	void Start() {
 		m_currAnimator = GetComponent<Animator>();
+		switch(m_chosenChar) {
+			case 0:
+				m_currAnimator.runtimeAnimatorController = Resources.Load("MageAnimationController") as RuntimeAnimatorController;
+				m_classAnimator = MageAnim.GetComponent<Animator>();
+				m_currAnimator.avatar = m_classAnimator.avatar;
+				m_overallMage.SetActive(true);
+				break;
+			 case 1:
+				m_currAnimator.runtimeAnimatorController = Resources.Load("WarriorAnimationController") as RuntimeAnimatorController;
+				m_classAnimator = WarriorAnim.GetComponent<Animator>();
+				m_currAnimator.avatar = m_classAnimator.avatar;
+				m_overallWarrior.SetActive(true);
+				break;
+			case 2:
+				m_currAnimator.runtimeAnimatorController = Resources.Load("RogueAnimationController") as RuntimeAnimatorController;
+				m_classAnimator = RogueAnim.GetComponent<Animator>();
+				m_currAnimator.avatar = m_classAnimator.avatar;
+				m_overallRogue.SetActive(true);
+				break;
+			case 3:
+				m_currAnimator.runtimeAnimatorController = Resources.Load("HunterAnimationController") as RuntimeAnimatorController;
+				m_classAnimator = HunterAnim.GetComponent<Animator>();
+				m_currAnimator.avatar = m_classAnimator.avatar;
+				m_overallHunter.SetActive(true);
+				break;
+			default:
+				m_currAnimator.runtimeAnimatorController = Resources.Load("MageAnimationController") as RuntimeAnimatorController;
+				m_classAnimator = MageAnim.GetComponent<Animator>();
+				m_currAnimator.avatar = m_classAnimator.avatar;
+				m_overallMage.SetActive(true);
+				break;
+		}
 	}
 
 	void Update() {
@@ -77,6 +114,7 @@ public class EnableNetworkScripts : NetworkBehaviour {
 	public void SetAnimator() {
 		for(int i = 0; i < m_animCounter; i++) {
 			m_netAnim.SetParameterAutoSend(i, true);
+			
 		}
 	}
 }
