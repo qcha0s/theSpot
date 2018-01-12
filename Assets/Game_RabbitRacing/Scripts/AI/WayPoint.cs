6d818d4913ce1f2588d3dbe58312b9732bdd5db7
KevinXue <ivexnuke@gmail.com>
2018-01-12 17:45:51 -0500

    Implemented position tracking

--- Assets/Game_RabbitRacing/Scripts/AI/WayPoint.cs
+++ Assets/Game_RabbitRacing/Scripts/AI/WayPoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class WayPoint : MonoBehaviour {
 	public WayPoint[] m_nextWayPoints;
-	private int m_wayPointNumber;
+	private int m_wayPointNumber = -1;
 	private static Color[] m_gizmoColours = {Color.red, Color.yellow, Color.green, Color.cyan, Color.blue, Color.magenta};
 	public int WayPointNumber{
 		get{
@@ -23,19 +23,38 @@ public class WayPoint : MonoBehaviour {
 			return retVal;
 		}
 	}
+	public WayPoint[] NextWayPoints{
+		get{
+			return m_nextWayPoints;
+		}
+	}
 	public Vector3 Point{
 		get{
 			return transform.position;
 		}
+	}
+	public bool HasNextPoint(WayPoint nextPoint){
+		bool foundPoint = false;
+		foreach(WayPoint point in m_nextWayPoints){
+			if(nextPoint == point){
+				foundPoint = true;
+				break;
+			}
+		}
+		return foundPoint;
 	}
 	private void OnTriggerEnter(Collider other) {
 		AIHandler kartBot = other.GetComponent<AIHandler>();
 		if(kartBot != null){
 			kartBot.Destination = NextWayPoint.Point;
 		}
+		PositionTracker position = other.GetComponent<PositionTracker>();
+		if(position){
+			position.CurrentWayPoint = this;
+		}
 	}
 	private void OnDrawGizmos() {
-		Gizmos.color = m_gizmoColours[m_wayPointNumber % m_gizmoColours.Length];
+		Gizmos.color = m_gizmoColours[Mathf.Abs(m_wayPointNumber) % m_gizmoColours.Length];
 		Gizmos.DrawSphere(transform.position, 1f);
 		foreach (WayPoint nextWayPoint in m_nextWayPoints){
 			if(nextWayPoint != null){
