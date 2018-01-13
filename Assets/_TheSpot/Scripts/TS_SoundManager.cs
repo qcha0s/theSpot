6514cd5d97febd027932c7e96ae2f4595cd48b13
Jason Berry <mrberry1990@gmail.com>
2018-01-12 21:08:54 -0500

    added singleton soundmanager to handle pausing/starting sound when leaving/entering theSpot

---
+++ Assets/_TheSpot/Scripts/TS_SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TS_SoundManager : MonoBehaviour {
+
+	private AudioSource m_sound;
+	private static TS_SoundManager m_instance = null;
+
+	public static TS_SoundManager Instance { get { return m_instance; } }
+
+	private void Awake() {
+		if (m_instance != null && m_instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			m_instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	private void Start() {
+		m_sound = GetComponent<AudioSource>();
+		StartMusic();
+	}
+
+	public void StopMusic() {
+		m_sound.Stop();
+	}
+
+	public void StartMusic() {
+		m_sound.Play();
+	}
+}
