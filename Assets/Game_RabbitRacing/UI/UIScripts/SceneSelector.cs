1cee664e801041d9b4a6e409ac7782b676f33117
Tom Sauer <tom@exluto.com>
2018-01-16 21:38:48 -0500

    checking out updated RR into proto (forgot to pull RR)

---
+++ Assets/Game_RabbitRacing/UI/UIScripts/SceneSelector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour {
+private int levelSelector;
+public GameObject charSelButton;
+public GameObject forestPreview;
+public GameObject snowPreview;
+public GameObject desertPreview;
+
+    void Start(){
+        levelSelector = -1;
+        charSelButton.SetActive(false);
+        forestPreview.SetActive(false);
+        snowPreview.SetActive(false);
+        desertPreview.SetActive(false);
+        
+    }
+
+    public void trackOne(){
+        levelSelector = 1;
+        forestPreview.SetActive(true);
+        snowPreview.SetActive(false);
+        desertPreview.SetActive(false);
+        charSelButton.SetActive(true);
+    }
+
+    public void trackTwo(){
+        levelSelector = 2;
+        snowPreview.SetActive(true);
+        forestPreview.SetActive(false);
+        desertPreview.SetActive(false);
+        charSelButton.SetActive(true);
+    }
+
+    public void trackThree(){
+        levelSelector = 3;
+        desertPreview.SetActive(true);
+        forestPreview.SetActive(false);
+        snowPreview.SetActive(false);
+        charSelButton.SetActive(true);
+    }
+
+    public void loadLevel(){
+        if(levelSelector != -1){
+        
+            SceneManager.LoadScene(levelSelector);
+        }
+    }
+}
